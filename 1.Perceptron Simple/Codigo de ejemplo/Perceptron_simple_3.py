318007855be960647ba73bb9746ede152e9f3393
pedroporo <pedroguillferri9@gmail.com>
2023-11-28 18:14:22 +0100

    vault backup: 2023-11-28 18:14:22

--- 1.Perceptron Simple/Codigo de ejemplo/Perceptron_simple_3.py
+++ 1.Perceptron Simple/Codigo de ejemplo/Perceptron_simple_3.py
@@ -2,3 +2,5 @@ class test:
     def __init__ (self, num_imputs,velocidadDeAprendizaje=0.1,interacciones=100):
         self.peso = [0.0] * num_imputs
         self.bias= 0.0
+        self.velocidadDeAprendizaje = velocidadDeAprendizaje
+        self.interacciones = interacciones

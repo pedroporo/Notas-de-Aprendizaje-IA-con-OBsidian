e54ff88d7518430a1ce2e79a4f0fb0aff1521ae8
pedroporo <pedroguillferri9@gmail.com>
2023-11-29 20:27:55 +0100

    vault backup: 2023-11-29 20:27:55

--- 1.Perceptron Simple/Codigo de ejemplo/Perceptron_simple_4.py
+++ 1.Perceptron Simple/Codigo de ejemplo/Perceptron_simple_4.py
@@ -1,1 +1,8 @@
-dhg
+class perceptronSimpleCode:
+    def __init__(self,num_inputs,velocidadDeAprendizaje=0.1,interacciones=100):
+        self.peso =[0.0]*num_inputs
+        self.bias=0.0
+        self.velocidadDeAprendizaje=velocidadDeAprendizaje
+        self.interacciones=interacciones
+    def prediccion(self,inputs):
+        peso_suma = sum()+self.bias

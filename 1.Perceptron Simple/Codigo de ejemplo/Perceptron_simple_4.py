7c38438d629ee19a271a1765fda7eb8d3d91437a
pedroporo <pedroguillferri9@gmail.com>
2023-12-01 18:00:36 +0100

    vault backup: 2023-12-01 18:00:36

--- 1.Perceptron Simple/Codigo de ejemplo/Perceptron_simple_4.py
+++ 1.Perceptron Simple/Codigo de ejemplo/Perceptron_simple_4.py
@@ -7,6 +7,7 @@ class perceptronSimpleCode:
     def prediccion(self,inputs):
         peso_suma = sum(w*x for w,x in zip(self.peso,inputs))+self.bias
         return 1 if peso_suma >=0 else 0
+    
     def entrenamiento(self,training_data):
         for interaccion in range(self.interacciones):
             for inputs,traget in training_data:
@@ -14,3 +15,12 @@ class perceptronSimpleCode:
                 error = traget -prediccion
                 self.pesos = [ w +self.velocidadDeAprendizaje*error*x for w * x in zip(self.peso,inputs)]
                 self.bias += self.velocidadDeAprendizaje *error
+
+training_data = [((0,0),0),((0,1),0),((1,0),0),((1,1),1)]
+
+perceptrons = perceptronSimpleCode(num_inputs=2)
+perceptrons.train(training_data)
+print(perceptron_and.predict((0, 0)))  # Salida esperada: 0
+print(perceptron_and.predict((0, 1)))  # Salida esperada: 0
+print(perceptron_and.predict((1, 0)))  # Salida esperada: 0
+print(perceptron_and.predict((1, 1)))  # Salida esperada: 1
